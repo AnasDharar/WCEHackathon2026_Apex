057195c460ffb1552b79b03700b93210abebdaea
Anas <86509653+AnasDharar@users.noreply.github.com>
2026-02-25 21:08:46 +0530

    api

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -14,6 +14,17 @@ const nextConfig = {
       }
     ]
   },
+  async rewrites() {
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/api/:path*",
+          destination: "http://127.0.0.1:8000/api/:path*",
+        },
+      ];
+    }
+    return [];
+  },
   reactCompiler: true,
 
 };
